e49e2fd1bbf9cfedea909d7bb897f9ef848ebd8c
ninthsun91 <ninthsun91@gmail.com>
2024-02-09 06:43:07 +0900

    12.3-7 Split to library crate

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, String> {
+        if args.len() != 3 {
+            let message = format!(
+                "2 arguments should be given but received {}",
+                args.len() - 1
+            );
+            return Err(message);
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text: \n{}", contents);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::error::Error;
-use std::{env, fs, process};
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,36 +11,8 @@ fn main() {
     println!("Seraching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application failed: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text: \n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, String> {
-        if args.len() != 3 {
-            let message = format!(
-                "2 arguments should be given but received {}",
-                args.len() - 1
-            );
-            return Err(message);
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
 }
