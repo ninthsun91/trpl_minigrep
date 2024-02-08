873840e930d0031784f3b8a760ad5b9ebf0a4fd9
ninthsun91 <ninthsun91@gmail.com>
2024-02-09 05:37:59 +0900

    12.3-2 Group configuration variables

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,25 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Seraching for {}", query);
-    println!("In file {}", file_path);
+    println!("Seraching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
 
     println!("With text: \n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
