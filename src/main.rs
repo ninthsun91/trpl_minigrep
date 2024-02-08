fedf368f7826cc715c9d73a3625bc00179ec451a
ninthsun91 <ninthsun91@gmail.com>
2024-02-09 05:29:29 +0900

    12.3-1 Extract argument parser

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Seraching for {}", query);
     println!("In file {}", file_path);
@@ -13,4 +11,11 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
 
     println!("With text: \n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
